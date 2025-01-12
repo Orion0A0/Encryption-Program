95c602399382c7e67f32f11a202653006d80913c
Orion0A0 <82244474+Orion0A0@users.noreply.github.com>
2025-01-11 22:29:25 -0800

    Add files via upload

---
+++ 3490_code/Main.java
@@ -0,0 +1,10 @@
+/** Function : it starts the program
+ * @author 3490
+ */
+class Main {
+    public static void main(String[] args) {  
+        UserInput r=new UserInput();
+        //start the program
+        r.start();
+  }
+}
